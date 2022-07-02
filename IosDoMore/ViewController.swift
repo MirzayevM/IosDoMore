8a27518efc88c98ecdb93369122e7545b9615920
Mirzabek <mirzabekmirzayev746@gmail.com>
2022-07-02 09:42:50 +0500

    Initial Commit

---
+++ IosDoMore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosDoMore
+//
+//  Created by Mirzabek on 02/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
